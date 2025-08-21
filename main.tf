e994d5b7b0fc751c80b9f3b3b49d63ce9d32ea63
wellington edesiri <139859013+wellington-wyzcode@users.noreply.github.com>
2025-08-21 17:40:44 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = "eu-west-2b"
+  region = "eu-west-2"
 }
 
 resource "aws_vpc" "devopsshack_vpc" {
@@ -14,7 +14,7 @@ resource "aws_subnet" "devopsshack_subnet" {
   count = 2
   vpc_id                  = aws_vpc.devopsshack_vpc.id
   cidr_block              = cidrsubnet(aws_vpc.devopsshack_vpc.cidr_block, 8, count.index)
-  availability_zone       = element(["eu-west-1a", "eu-west-1b"], count.index)
+  availability_zone       = element(["eu-west-2a", "eu-west-2b"], count.index)
   map_public_ip_on_launch = true
 
   tags = {
